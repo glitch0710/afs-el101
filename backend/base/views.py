027b6709921bbd77078f01c91d3060005889fc50
Rhogiel Andoy <theglitch0710@gmail.com>
2023-03-28 16:43:18 +0800

    #8 DRF setup

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_routes(request):
+    return Response('hello')
