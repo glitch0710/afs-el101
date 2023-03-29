e4a6d56c6b51136d703a49d5d5ca8b2d902ba358
Rhogiel Andoy <theglitch0710@gmail.com>
2023-03-30 00:53:34 +0800

    #14 implement react-redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,15 +2,18 @@ import { applyMiddleware, combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./reducers/productReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer,
+});
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = configureStore(
-  {reducer: reducer},
+  { reducer: reducer },
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
