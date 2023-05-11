d4ee87ec098b1b83041fbaaf4dc728a0306d4753
Rhogiel Andoy <theglitch0710@gmail.com>
2023-05-11 22:19:03 +0800

    #21 setup JWT authentication

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='products'),
-    path('product/<str:pk>', views.get_product, name='product')
+    path('product/<str:pk>', views.get_product, name='product'),
+
+    # JWT
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
