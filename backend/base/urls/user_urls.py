e13fa475cef0029a585a448a6fe15679aa6523a4
Rhogiel Andoy <theglitch0710@gmail.com>
2023-05-20 15:38:01 +0800

    #29 added user profile screen

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.get_users, name='get_users'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.get_user_profile, name='get_user_profile'),
+    path('profile/update', views.update_user_profile, name='update_user_profile'),
     path('register/', views.register_user, name='register_user'),
 ]
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,12 +6,15 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { loading: true};
+      return { loading: true };
 
     case USER_LOGIN_SUCCESS:
       return { loading: false, userInfo: action.payload };
@@ -30,7 +33,7 @@ export const userLoginReducer = (state = {}, action) => {
 export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_REGISTER_REQUEST:
-      return { loading: true};
+      return { loading: true };
 
     case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: action.payload };
@@ -46,3 +49,18 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 };
 
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+      
+    default:
+      return state;
+  }
+};
